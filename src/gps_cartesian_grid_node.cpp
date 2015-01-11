ff3519497ce62358691055e344f1cc7dabad73cb
Justin Eskesen <justin.eskesen@gmail.com>
2015-01-10 17:21:33 -0800

    Added tf2 broadcaster for publishing the transform between the world frame & the robot's base link frame.  Currently, only translation is implemented.

--- src/gps_cartesian_grid_node.cpp
+++ src/gps_cartesian_grid_node.cpp
@@ -1,6 +1,8 @@
 
 #include <ros/ros.h>
 #include <gps_cartesian_grid/FixToPoint.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <GeographicLib/LocalCartesian.hpp>
 
 class GpsCartesianGridNode
@@ -10,9 +12,22 @@ public:
 private:
 	geometry_msgs::Point fixToPoint(sensor_msgs::NavSatFix gps_fix);
 	bool fixToPointServiceCall(gps_cartesian_grid::FixToPoint::Request &req, gps_cartesian_grid::FixToPoint::Response &point);
+	void newNavSatFix(sensor_msgs::NavSatFix newFix){gps_fix_ = newFix;  publishTf();}
+	void publishTf();
 	ros::NodeHandle nh_, private_nh_;
+
+	// for service
 	ros::ServiceServer fixToPointService_;
 
+	// for tf
+	tf2_ros::TransformBroadcaster tf_broadcaster_;
+	std::string global_frame_, robot_frame_;
+
+	std::string gps_topic_;
+	ros::Subscriber gps_sub_;
+	sensor_msgs::NavSatFix gps_fix_;
+
+	// thirdparty GeographicLib
 	GeographicLib::LocalCartesian grid_;
 };
 
@@ -28,10 +43,15 @@ GpsCartesianGridNode::GpsCartesianGridNode() : nh_(),
 	}
 
 	private_nh_.param("origin_altitude", alt, 0.0);
+	private_nh_.param("global_frame", global_frame_, std::string("/world"));
+	private_nh_.param("robot_frame", robot_frame_, std::string("base_link"));
+
+	private_nh_.param("gps_fix_topic", gps_topic_, std::string("fix"));
 
 	grid_.Reset(lat,lon,alt);
 
 	fixToPointService_ = nh_.advertiseService("fix_to_point", &GpsCartesianGridNode::fixToPointServiceCall, this);
+	gps_sub_ = nh_.subscribe(gps_topic_, 10, &GpsCartesianGridNode::newNavSatFix, this);
 }
 
 geometry_msgs::Point GpsCartesianGridNode::fixToPoint(sensor_msgs::NavSatFix gps_fix)
@@ -48,13 +68,34 @@ bool GpsCartesianGridNode::fixToPointServiceCall(gps_cartesian_grid::FixToPoint:
 	return true;
 }
 
+void GpsCartesianGridNode::publishTf()
+{
+	geometry_msgs::TransformStamped transformStamped;
 
+	transformStamped.header.stamp = ros::Time::now();
+	transformStamped.header.frame_id = global_frame_;
+	transformStamped.child_frame_id = robot_frame_;
+
+	geometry_msgs::Point point = fixToPoint(gps_fix_);
+	transformStamped.transform.translation.x = point.x;
+	transformStamped.transform.translation.y = point.y;
+	transformStamped.transform.translation.z = point.z;
+
+	/*
+	tf2::Quaternion q;
+	q.setRPY(0, 0, msg->theta);
+	transformStamped.transform.rotation.x = q.x();
+	transformStamped.transform.rotation.y = q.y();
+	transformStamped.transform.rotation.z = q.z();
+	transformStamped.transform.rotation.w = q.w();
+	*/
+
+	tf_broadcaster_.sendTransform(transformStamped);
+}
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "gps_cartesian_grid_node");
-
 	ros::spin();
-
 	return 0;
 }
